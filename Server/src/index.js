73b2fa3e76999da5795613b3e69a2285227c9591
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2025-03-07 14:52:25 +0200

    Revert "Update CORS origin to Vercel deployment URL"
    This reverts commit fc407a3313869b2205b6944f4718de2e0acf1f85.

--- Server/src/index.js
+++ Server/src/index.js
@@ -26,7 +26,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://tlgram-mohamedabdelaiem-mohamedabdelaiems-projects.vercel.app/",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
