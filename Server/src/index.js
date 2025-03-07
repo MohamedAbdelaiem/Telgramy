7c90dd343005d4f656f13c2df7af92fb50f3fe55
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2025-03-07 14:52:22 +0200

    Revert "Fix CORS origin URL by removing trailing whitespace"
    This reverts commit 8edb41da99ef45e3b1f665cfbaeefdffb3752065.

--- Server/src/index.js
+++ Server/src/index.js
@@ -26,7 +26,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://tlgram-mohamedabdelaiem-mohamedabdelaiems-projects.vercel.app  ",
+    origin: "https://tlgram-mohamedabdelaiem-mohamedabdelaiems-projects.vercel.app/",
     credentials: true,
   })
 );
