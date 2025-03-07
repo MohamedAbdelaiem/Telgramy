00d067189455749d3491ba7e39f75f310dd7823b
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2025-03-07 14:00:09 +0200

    `Update axios and useAuthStore to use Vercel API URL in production`

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: import.meta.env.MODE==='development'?'http://localhost:3000/api':"/api",
+    baseURL: import.meta.env.MODE==='development'?'http://localhost:3000/api':"/https://tlgram-ddoouxioc-mohamedabdelaiems-projects.vercel.app/api",
     withCredentials: true
 });
 
